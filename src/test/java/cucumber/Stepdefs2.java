b5b9de785267b233d7d0a43303d6f72e59cdd1e4
ignamarga1 <0619967319@uma.es>
2023-05-08 19:50:44 +0200

    añadida prueba borrar_anuncio

---
+++ src/test/java/cucumber/Stepdefs2.java
@@ -0,0 +1,70 @@
+package cucumber;
+
+import board.Advertisement;
+import board.AdvertisementBoard;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs2 {
+    Advertisement advertisement;
+    AdvertisementBoard advertisementBoard;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+
+    @Given("hay un tablón de anuncios creado")
+    public void adb_created() {
+        advertisementBoard = new AdvertisementBoard();
+    }
+
+    @And("el tablón tiene al menos un anuncio del usuario")
+    public void adb_has_at_least_one_advertisement() {
+        advertisement = new Advertisement("title", "text", "user");
+        advertisementBoard.publish(advertisement);
+    }
+
+    @When("el usuario borra un anuncio suyo que está en el tablón")
+    public void user_deletes_advertisement() {
+        advertisementBoard.deleteAdvertisement("title", "user");
+    }
+
+    @Then("el anuncio es borrado del tablón")
+    public void advertisement_is_deleted() {
+        Optional<Advertisement> expectedValue = Optional.empty();
+        Optional<Advertisement> actualValue = advertisementBoard.findByTitle("title");
+        assertEquals(expectedValue, actualValue);
+    }
+    @Then("el numero de anuncios publicados disminuye")
+    public void number_of_advertisements_published_decreases() {
+        int expectedValue = advertisementBoard.numberOfPublishedAdvertisements()-1;
+        int actualValue = advertisementBoard.numberOfPublishedAdvertisements();
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Then("el sistema avisa al usuario de que se ha borrado el anuncio")
+    public void aVoid() {
+
+    }
+}
