cc587e1df3d2e8b9a64b7898d8409c788bdef1a0
ignamarga1 <0619967319@uma.es>
2023-05-16 19:28:32 +0200

    new class Publisher

---
+++ src/main/java/board/Publisher.java
@@ -0,0 +1,11 @@
+package board;
+
+public class Publisher {
+    public String name;
+    public float funds;
+
+    public Publisher(String name, float funds) {
+        this.name = name;
+        this.funds = funds;
+    }
+}
